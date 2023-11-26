92f0ab9166f38ccea3c3b68a15758a3159d99ea5
softprops <d.tangren@gmail.com>
2023-11-26 12:43:55 -0500

    clean

--- src/main.zig
+++ src/main.zig
@@ -34,38 +34,16 @@ fn impl(
         .debug => "\x1b[95m",
     } ++ comptime level.asText() ++ "\x1b[0m " ++ scope_prefix;
     nosuspend writer.print(prefix ++ format ++ "\n", args) catch return;
-    // var msg: [std.fmt.count(format, args)]u8 = undefined;
-    // _ = std.fmt.bufPrint(&msg, format, args) catch |err| {
-    //     std.debug.print("caught err writing to buffer {any}", .{err});
-    // };
-    // nosuspend std.json.stringify(.{
-    //     .level = level.asText(),
-    //     .msg = msg,
-    //     .scope = @tagName(scope),
-    // }, .{}, writer) catch |err| {
-    //     std.debug.print("caught err writing json {any}", .{err});
-    // };
-    // writer.writeAll("\n") catch return;
 }
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    //const stdout_file = std.io.getStdOut().writer();
-    //var bw = std.io.bufferedWriter(stdout_file);
-    //const stdout = bw.writer();
-
-    //try stdout.print("Run `zig build test` to run the tests.\n", .{});
     std.log.debug("test 1", .{});
     std.log.info("test 2", .{});
     std.log.warn("test 3", .{});
     std.log.err("test 4", .{});
-
-    //try bw.flush(); // don't forget to flush!
 }
 
 test "simple test" {
@@ -78,8 +56,6 @@ test "simple test" {
     const actual = try list.toOwnedSlice();
     defer allocator.free(actual);
     try std.testing.expectEqualStrings(
-    // \\{"level":"info","msg":"test","scope":"bar"}
-    // \\
         \\info (bar): test
         \\
     , actual);
